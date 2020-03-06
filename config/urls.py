b564876cb376ccdd402b4b93a46a4f4ba5828d87
Brian Bonsignore <bonsignore.brian@gmail.com>
2020-03-06 12:29:37 -0500

    Partial day checker

--- config/urls.py
+++ config/urls.py
@@ -8,10 +8,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('registration.backends.default.urls')),
     path('', views.habits, name='habits'),
-    path('habit_records/<int:pk>', views.habit_records, name='habit_records'),
-    path('add_habit/', views.add_habit, name='add_habit'),
-    path('add_record/', views.add_record, name='add_record'),
-    path('habit_record/<int:pk>', views.habit_record, name='habit_record'),
+    path('habit-records/<int:pk>', views.habit_records, name='habit_records'),
+    path('add-habit/', views.add_habit, name='add_habit'),
+    path('add-record/', views.add_record, name='add_record'),
+    path('add-habit-record/<int:pk>', views.habit_record, name='add_habit_record'),
 
 ]
 
